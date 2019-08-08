53a7fdaa32c4791dd9869ea7e2346cb5db58facf
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-08 20:26:40 +0100

    WIP: added method to increase strikes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>app placeholder</p>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      balls: 0,
+      strikes: 0
+    }
+  }
 
+  // method to record a strike
+  recordStrike = () => {
+    this.setState({ strikes: (this.state.strikes + 1) })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.strikes}</p>
+        <button onClick={this.recordStrike}>Strike!</button>
+      </div>
+    );
+  }
+}
 export default App;
