09da25f5aaa6701b08d7cf674610348398f31ec3
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-08 23:12:44 +0100

    WIP: added tests to click buttons

--- src/App.test.js
+++ src/App.test.js
@@ -12,16 +12,40 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('displays the count of balls', () => {
+it('displays the count of balls and strikes', () => {
   const { getByTestId } = render(<App />)
   expect(getByTestId('ballsCount').textContent).toBe('0')
+  expect(getByTestId('strikesCount').textContent).toBe('0')
 })
 
-it('increments count of balls on click of ball function', () => {
+it('increments count of balls by 1 on click of ball button', () => {
   const { getByTestId } = render(<App />)
   fireEvent.click(getByTestId('ball'))
   expect(getByTestId('ballsCount').textContent).toBe('1')
 })
+
+it('resets count of balls and strikes to 0 on click of hit button', () => {
+  const { getByTestId } = render(<App />)
+  fireEvent.click(getByTestId('hit'))
+  expect(getByTestId('ballsCount').textContent).toBe('0')
+  expect(getByTestId('strikesCount').textContent).toBe('0')
+})
+
+it('if strikes count = 0, clicking foul button sets strike count to 1, and increments ball count by one', () => {
+  const { getByTestId } = render(<App />)
+  fireEvent.click(getByTestId('foul'))
+  expect(getByTestId('strikesCount').textContent).toBe('1')
+  expect(getByTestId('ballsCount').textContent).toBe('1')
+})
+
+it('increments count of strikes and balls by 1 on click of strike button', () => {
+  const { getByTestId } = render(<App />)
+  fireEvent.click(getByTestId('strike'))
+  expect(getByTestId('strikesCount').textContent).toBe('1')
+  expect(getByTestId('ballsCount').textContent).toBe('1')
+})
+
+
 
 
 
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Dashboard = (props) => {
   return (
     <>
-      <button onClick={props.recordStrike}>Strike</button>
+      <button data-testid = 'strike' onClick={props.recordStrike}>Strike</button>
       <button data-testid = 'ball' onClick={props.recordBall}>Ball</button>
-      <button onClick={props.recordFoul}>Foul</button>
-      <button onClick={props.recordHit}>Hit</button>
+      <button data-testid = 'foul' onClick={props.recordFoul}>Foul</button>
+      <button data-testid = 'hit' onClick={props.recordHit}>Hit</button>
     </>
   )
 }
